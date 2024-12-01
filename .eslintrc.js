c9e91930bd81405205e309a39a559e6febc47121
hooneun <kimhoon0316@gmail.com>
2024-12-01 20:03:01 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled from "@emotion/styled";
+import { Button } from "./components/Button";
+import { Label } from "./components/Label";
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 32px;
+`;
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const sub = () => {
+    setCounter(counter - 1);
+  };
+
+  const add = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Counter App</Title>
+      <Content>
+        <Button label="-" onClick={sub} />
+        <Label data={counter} />
+        <Button label="+" onClick={add} />
+      </Content>
+    </Container>
   );
 }
 
